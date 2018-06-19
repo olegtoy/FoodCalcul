fd1e257867c0d91fa221d9ff0010ae55d17d133c
Oleg Toygildin <toyoa@ya.ru>
2018-06-19 19:15:16 +0300

    Final release

--- app/src/main/java/com/codingdemos/tablayout/All_product/StatusFragment.java
+++ app/src/main/java/com/codingdemos/tablayout/All_product/StatusFragment.java
@@ -34,7 +34,6 @@ public class StatusFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         setHasOptionsMenu(true);
         View v = inflater.inflate(R.layout.fragment_call, container, false);
         ViewGroup footer = (ViewGroup) inflater.inflate(R.layout.status_footer, listView, false);
